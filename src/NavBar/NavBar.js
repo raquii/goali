4ee1cb78b59bef1072ea3c7f7cca672b2ad24ffd
raquel <raquelanaroman@gmail.com>
2021-08-30 16:55:05 -0400

    Routing with react router and responsive UI

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,18 +2,34 @@ import './NavBar.css';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-function NavBar(){
+function NavBar() {
     const [open, setOpen] = useState(false)
     const handleClick = () => setOpen(!open);
     const closeMobile = () => setOpen(false);
 
-    return(
-        <Nav id='navbar'>
-            <NavLink exact to="/">Home</NavLink>
-            <NavLink to="">About</NavLink>
-            <NavLink to="">Sign Up</NavLink>
-            <NavLink to="">Sign In</NavLink>
-        </Nav>
+    return (
+        <nav id='navbar' className={ open ? 'responsive' : ''}>
+            <div id='home-nav'>
+                <NavLink exact to="/" className='link'>Home</NavLink>
+            </div>
+            <ul id='ul-nav'>
+                <a href="#" className="icon link" onClick={handleClick}>
+                    { open ? <i className='fa fa-close'/> : <i className="fa fa-bars"/>}
+                </a>
+                <li className='li-nav'>
+                    <NavLink exact to="/" className='link'>Home</NavLink>
+                </li>
+                <li className='li-nav'>
+                    <NavLink to="/about" className='link'>About</NavLink>
+                </li>
+                <li className='li-nav'>
+                    <NavLink to="/signup" className='link'>Sign Up</NavLink>
+                </li>
+                <li className='li-nav'>
+                    <NavLink to="/signin" className='link'>Sign In</NavLink>
+                </li>
+            </ul>
+        </nav >
     )
 }
 
