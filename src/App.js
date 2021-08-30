d1d3678b46029f505cd594adb5a6739a3dcb2559
raquel <raquelanaroman@gmail.com>
2021-08-30 16:55:16 -0400

    routing with react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { Switch, Route } from 'react-router';
+
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/about">
+          <h1>about</h1>
+        </Route>
+        <Route path="/signup">
+          <h1>signup</h1>
+        </Route>
+        <Route path="/signin">
+          <h1>signin</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
