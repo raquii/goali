7a8cfac8f08329bd5f66d144c2f0477a35af756f
raquel <raquelanaroman@gmail.com>
2021-08-31 11:20:15 -0400

    rearrange routes and add home + sign in component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import './App.css';
 import { Switch, Route } from 'react-router';
 
 import NavBar from './NavBar/NavBar';
+import Home from './Home/Home';
+import SignIn from './signin/SignIn';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <h1>Home</h1>
-        </Route>
-        <Route path="/about">
-          <h1>about</h1>
-        </Route>
-        <Route path="/signup">
-          <h1>signup</h1>
-        </Route>
-        <Route path="/signin">
-          <h1>signin</h1>
-        </Route>
-      </Switch>
+      <main id='main-content'>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <h1>about</h1>
+          </Route>
+          <Route path="/signup">
+            <h1>signup</h1>
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
