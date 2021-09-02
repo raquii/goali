a83fd5e2649ab419e5c88f17e5c19c510bda2bb1
raquel <raquelanaroman@gmail.com>
2021-09-01 21:48:04 -0400

    add error rendering

--- src/ForgotPassword/ForgotPassword.js
+++ src/ForgotPassword/ForgotPassword.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import Button from "../Button/Button";
 
-function ForgotPassword({setForgot}){
-    
+function ForgotPassword({setForgot, errors, setErrors}){
     const [email, setEmail] = useState('');
 
+    //need to add something that says email was sent.
     function handleRecoverPassword(){
-        console.log(`send me my password to: ${email}`)
+        if(emailValid()){
+            console.log(`send me my password to: ${email}`)
+            setEmail('')
+        }else{
+            console.log('invalid')
+        }
+    }
+    // simple valid email check -- should change to fetch to back end instead of regex later
+    function emailValid(){
+        if(/\S+@\S+\.\S+/g.test(email)){
+            setErrors('')
+            return true
+        }else{
+            setErrors('Email is not valid!')
+        }
+        return false
     }
 
     return (
@@ -16,9 +31,15 @@ function ForgotPassword({setForgot}){
             <input
                 id='email'
                 type='email'
+                className={errors.length > 0 ? 'error-input' : undefined}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.length > 0 && 
+                <span className='error-msg'>
+                    <i className="fas fa-exclamation-triangle" /> {errors}
+                </span>
+            }
                 <Button
                     type='submit'
                     text='Send Password Recovery Email'
