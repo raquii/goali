67ea89085f5b157828934e3fb235e435d4c701b8
raquel <raquelanaroman@gmail.com>
2021-09-03 18:33:01 -0400

    create persistent user session logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Switch, Route } from 'react-router';
+import { useState, useEffect } from 'react';
 
 import NavBar from './NavBar/NavBar';
 import Home from './Home/Home';
@@ -7,9 +8,25 @@ import SignIn from './signin/SignIn';
 import SignUp from './signup/SignUp';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('/user')
+    .then(r=>{
+      if(r.ok){
+        r.json()
+        .then(user=>setUser(user));
+      }else{
+        r.json()
+        .then(console.log(r))
+      }
+    });
+  }, [])
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar user={user} onLogout={setUser}/>
+      {user&&<h1>You're logged in!</h1>}
       <main id='main-content'>
         <Switch>
           <Route exact path="/">
@@ -22,7 +39,7 @@ function App() {
             <SignUp />
           </Route>
           <Route path="/signin">
-            <SignIn />
+            <SignIn onSignIn={setUser}/>
           </Route>
         </Switch>
       </main>
