75a1f866f6b57f4e9b4bb05a16c0139361d3797e
raquel <raquelanaroman@gmail.com>
2021-09-07 19:20:54 -0400

    reorganize file structure

---
+++ src/public/Public.js
@@ -0,0 +1,32 @@
+import { Switch, Route } from 'react-router';
+
+import NavBar from './navbar/NavBar';
+import SignIn from './signin/SignIn';
+import SignUp from './signup/SignUp';
+import Home from './home/Home';
+
+function Public({ onSignIn }) {
+  return (
+    <>
+      <NavBar />
+      <main id='main-content'>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <h1>about</h1>
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/signin">
+            <SignIn onSignIn={onSignIn} />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default Public;
