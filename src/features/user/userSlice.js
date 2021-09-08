4200f1d8e4901ed69c5798481d7b3b32aff94be8
raquel <raquelanaroman@gmail.com>
2021-09-08 00:24:33 -0400

    add extra actions for login thunk

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,25 +1,38 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../api";
 
+const initialState = {
+    username: "",
+    name: "",
+    email: "",
+    birthday: "",
+};
 
 export const userSlice = createSlice({
     name: "user",
-    initialState: {
-        id: null,
-        username: "",
-        name:"",
-        email: "",
-        birthday: "",
-        isFetching: false,
-        isSuccess: false,
-        isError: false,
-        errorMessage: "",
-    },
+    initialState,
     reducers: {
-        
+        logout: () => initialState,
     },
-    extraReducers:{
-        
+    extraReducers: (builder)=>{
+        builder
+            .addMatcher(api.endpoints.login.matchPending, (state, action) => {
+                console.log('pending', action);
+            })
+            .addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
+                console.log('fulfilled', action);
+                state.name = action.payload.name;
+                state.username = action.payload.username;
+                state.email = action.payload.email;
+                state.birthday = action.payload.birthday;
+            })
+            .addMatcher(api.endpoints.login.matchRejected, (state, action) => {
+                console.log('rejected', action);
+            });
 
     }
 })
 
+export const { logout } = userSlice.actions;
+
+export default userSlice.reducer;
