d02a0db3a6efc2e15a985d80b622de4e2242d9ef
raquel <raquelanaroman@gmail.com>
2021-09-08 14:57:06 -0400

    update styling for sidebar

--- src/private/sidebar/Sidebar.js
+++ src/private/sidebar/Sidebar.js
@@ -17,6 +17,7 @@ export default function Sidebar() {
     async function signOut() {
         try {
             await logout()
+            setDisplayConfirm(false)
             history.push("/")
         } catch (error) {
             console.log(error)
@@ -34,9 +35,12 @@ export default function Sidebar() {
             }
             <nav id='sidebar' className={open ? 'responsive' : ''}>
                 <ul id='ul-side'>
-                    <button className="icon side-link" onClick={() => setOpen(!open)}>
+                    <button className="side-icon" onClick={() => setOpen(!open)}>
                         {open ? <i className='fa fa-close' /> : <i className="fa fa-bars" />}
                     </button>
+                    <li className='li-side' id='home-li'>
+                        <NavLink to="/dashboard" className='side-link'>Home</NavLink>
+                    </li>
                     <li className='li-side'>
                         <NavLink to="/profile" className='side-link'>Profile</NavLink>
                     </li>
@@ -49,14 +53,13 @@ export default function Sidebar() {
                     <li className='li-side'>
                         <NavLink to="/settings" className='side-link'>Settings</NavLink>
                     </li>
-                    <li className='li-side'>
                         <Button
-                            className='sign-out-btn'
+                            className='sign-out-btn button'
                             clickHandler={() => setDisplayConfirm(true)}
                             text='Sign Out'
                             type='button'
                         />
-                    </li>
+
                 </ul>
             </nav >
         </>
