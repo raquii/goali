f94cde0d482a4d094bef1caee20b187e1a294bda
raquel <raquelanaroman@gmail.com>
2021-09-08 10:47:58 -0400

    add elements to assist in styling

--- src/private/alert/Alert.js
+++ src/private/alert/Alert.js
@@ -1,22 +1,26 @@
+import './Alert.css'
 import Button from "../../components/button/Button";
-
 
 export default function Alert({ message = "Are you sure?", confirmHandler, cancelHandler }) {
     return (
-        <div className='alert'>
-            {message}
-            <Button
-                className="confirm-action"
-                type="button"
-                clickHandler={confirmHandler}
-                text="OK"
-            />
-            <Button
-                className="cancel-action"
-                type="button"
-                clickHandler={cancelHandler}
-                text="Cancel"
-            />
+        <div className='modal'>
+            <div className='alert'>
+                {message}
+                <div className='button-container'>
+                    <Button
+                        className="confirm button"
+                        type="button"
+                        clickHandler={confirmHandler}
+                        text="OK"
+                    />
+                    <Button
+                        className="cancel button"
+                        type="button"
+                        clickHandler={cancelHandler}
+                        text="Cancel"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
