fdbe9c921dad67320706b2185c2466ce71c1922b
raquel <raquelanaroman@gmail.com>
2021-09-12 13:21:18 -0400

    add interactive detail display to habit card

--- src/private/habits/HabitCard.js
+++ src/private/habits/HabitCard.js
@@ -1,36 +1,66 @@
 import { useState } from 'react';
-import Toggle from 'react-toggle';
+import dayjs from 'dayjs';
+
+import Button from '../../components/button/Button';
 import './HabitCard.css';
 import HabitForm from './HabitForm';
 
 
-export default function HabitCard({habit}) {
+export default function HabitCard({ habit }) {
     const [showForm, setShowForm] = useState(false);
+    const [showDetails, setShowDetails] = useState(false);
 
-    const { id,
+    const {
+        id,
         name,
         description,
         periodicity,
         frequency,
         private_habit,
         archived,
-        total_times} = habit;
+        total_times
+    } = habit;
 
+    const habitDetails = () => {
+        return (
+            <>
+                <p className='habit-frequency'>
+                    My goal is to {name.toLowerCase()} {frequency > 1 ? `${frequency} times` : `once`} a {periodicity}.
+                </p>
+                <h3 className='habit-count'>
+                    Total Times: <span className='habit-highlight'>{total_times}</span>
+                </h3>
+                {description.length > 0 &&
+                    <p className='habit-description'>
+                        <strong>Description:</strong> {description}
+                    </p>}
+            </>
+        )
+    }
     return (
         <>
-        {showForm && <HabitForm setShowForm={setShowForm} habit={habit} formPurpose='Edit'/>}
-        <div className='habit-card' id={id}>
-            <h2 className='habit-name'>{name}</h2>
-            <p className='habit-description'>{description}</p>
-            <h3 className='habit-count'>{total_times} times</h3>
-            
-            <p className='habit-frequency'>My goal is to {name.toLowerCase()} {frequency} {frequency > 1 ? "times" : "time"} a {periodicity}.</p>
-            <div className='habit-button-container'>
-                <button type='button' className='habit-button' onClick={() => setShowForm(true)}>
-                    <i className='fas fa-edit' />
-                </button>
+            {showForm && <HabitForm setShowForm={setShowForm} habit={habit} formPurpose='Edit' />}
+            <div className='habit-card' id={id}>
+                <h2 className='habit-name'>{name} {private_habit && <i title="Private habit" className="fas fa-lock"/>}</h2>
+                {showDetails && habitDetails()}
+                <Button
+                    className='show-details'
+                    clickHandler={() => setShowDetails(!showDetails)}
+                    text={showDetails ? <i className="fas fa-chevron-up" /> : <i className='fas fa-ellipsis-h' />}
+                />
+                <div className='habit-button-container'>
+                    {/* <Button
+                        className='completed habit-button'
+                        clickHandler={() => console.log('finished')}
+                        text={<i className='fas fa-ellipsis-h' />}
+                    /> */}
+                    <Button
+                        className='habit-button'
+                        clickHandler={() => setShowForm(true)}
+                        text={<i className='fas fa-edit' />}
+                    />
+                </div>
             </div>
-        </div>
         </>
     )
 }
