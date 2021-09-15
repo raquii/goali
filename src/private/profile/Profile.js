2d9b3a0406d109fabf72a7b11d92ac0a88e8d77f
raquel <raquelanaroman@gmail.com>
2021-09-15 16:49:36 -0400

    add logic for profile form display

--- src/private/profile/Profile.js
+++ src/private/profile/Profile.js
@@ -1,4 +1,10 @@
-import './Profile.css'
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+import './Profile.css';
+import Button from '../../components/button/Button';
+import ProfileForm from './ProfileForm';
+
 
 export default function Profile({
     username,
@@ -7,42 +13,59 @@ export default function Profile({
     location,
     profile_picture,
 }) {
+    const [showForm, setShowForm] = useState(false)
+    const currentUser = useSelector(state => state.user.username)
+
     return (
         <div id="profile-container">
+            {showForm && <ProfileForm setShowForm={setShowForm} />}
+            <Button
+                type='button'
+                className='profile-button profile-icon'
+                clickHandler={() => console.log('I want see their profile')}
+                text={<i className='fas fa-user-circle fa-8x' />}
+            />
+            {username === currentUser &&
+                <Button
+                    className='edit-button profile-edit'
+                    clickHandler={() => setShowForm(true)}
+                    text={<i className='fas fa-edit' />}
+                />
+            }
             <table>
                 <tbody>
-                <tr>
-                    <th>
-                        Username:
-                    </th>
-                    <td>
-                        {username}
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        Name:
-                    </th>
-                    <td>
-                        {name}
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        Location:
-                    </th>
-                    <td>
-                        {location}
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        Bio:
-                    </th>
-                    <td>
-                        {bio}
-                    </td>
-                </tr>
+                    <tr>
+                        <th>
+                            Username:
+                        </th>
+                        <td>
+                            {username}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Name:
+                        </th>
+                        <td>
+                            {name}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Location:
+                        </th>
+                        <td>
+                            {location}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Bio:
+                        </th>
+                        <td>
+                            {bio}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
