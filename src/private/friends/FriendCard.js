651f089f955650ceb9d4b5c4af8fa5baeaf00a76
raquel <raquelanaroman@gmail.com>
2021-09-15 16:48:07 -0400

    add correct methods for removing friends

--- src/private/friends/FriendCard.js
+++ src/private/friends/FriendCard.js
@@ -6,20 +6,20 @@ import Alert from "../alert/Alert";
 export default function FriendCard({ id, friendId, username, name, removeFriend }) {
     const [showAlert, setShowAlert] = useState(false);
 
-    function deleteFriend(id){
+    function deleteFriend(id) {
         fetch(`http://localhost:3000/friendships/${id}`, {
             method: 'DELETE',
-            credentials:'include',
-            headers:{
+            credentials: 'include',
+            headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({friend_id: friendId})
+            body: JSON.stringify({ friend_id: friendId })
         })
-        .then(r=>r.json())
-        .then(()=>{
-            setShowAlert(false)
-            removeFriend(friends=>friends.filter(f => f.id !== id))
-        })
+            .then(r => r.json())
+            .then(() => {
+                setShowAlert(false)
+                removeFriend(friends => friends.filter(f => f.id !== id))
+            })
     }
 
     return (
@@ -27,28 +27,33 @@ export default function FriendCard({ id, friendId, username, name, removeFriend 
             {showAlert && <Alert
                 message={`Are you sure you'd like to remove ${name} as a friend?`}
                 confirmHandler={() => deleteFriend(id)}
-                cancelHandler={()=>setShowAlert(false)}
+                cancelHandler={() => setShowAlert(false)}
             />
             }
             <div key={username} className='friend-card'>
                 <Button
                     type='button'
-                    className='friend-button'
-                    clickHandler={() => console.log('I want to edit this habit')}
+                    className='profile-button friend-icon'
+                    clickHandler={() => console.log('I want see their profile')}
                     text={<i className='fas fa-user-circle fa-8x' />}
                 />
-                <h2>{name} • <em>{username}</em></h2>
+                <h2>
+                    {name}
+                    <br />
+                
+                    <span className='friend-username'>{username}</span>
+                </h2>
                 <div className='friend-button-container'>
                     <Button
                         type='button'
-                        className='friend-button'
-                        clickHandler={() => console.log('I want to edit this habit')}
+                        className='friend-button message'
+                        clickHandler={() => console.log('I want to message my friend')}
                         text={<i className='fas fa-envelope' />}
                     />
                     <Button
                         type='button'
                         className='friend-button delete'
-                        clickHandler={()=>setShowAlert(true)}
+                        clickHandler={() => setShowAlert(true)}
                         text={<i className='fas fa-user-slash' />}
                     />
                 </div>
