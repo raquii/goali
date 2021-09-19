c349e124fbcfe34603a523a2d565c6c5aec90385
raquel <raquelanaroman@gmail.com>
2021-09-19 14:50:20 -0400

    fix disasterous reducer logic in Immer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -7,12 +7,12 @@ const initialState = {
     email: "",
     birthday: "",
     profile: {
-        id:"",
+        id: "",
         location: "",
         bio: "",
-        profile_picture:""
+        profile_picture: ""
     },
-    habits:[]
+    habits: []
 };
 
 export const slice = createSlice({
@@ -21,21 +21,15 @@ export const slice = createSlice({
     reducers: {
 
     },
-    extraReducers: (builder)=>{
+    extraReducers: (builder) => {
         builder
             .addMatcher(api.endpoints.login.matchPending, (state, action) => {
                 console.log('pending', action);
             })
             .addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
                 console.log('fulfilled-login', action);
-                state.name = action.payload.name;
-                state.username = action.payload.username;
-                state.email = action.payload.email;
-                state.birthday = action.payload.birthday;
-                state.profile.location = action.payload.profile.location;
-                state.profile.bio = action.payload.profile.bio;
-                state.profile.profile_picture = action.payload.profile.profile_picture;
-                state.habits = action.payload.habits;
+                const { name, username, email, birthday, profile, habits } = action.payload;
+                Object.assign(state, { name, username, email, birthday, profile, habits })
             })
             .addMatcher(api.endpoints.login.matchRejected, (state, action) => {
                 console.log('rejected', action);
@@ -44,24 +38,24 @@ export const slice = createSlice({
                 console.log('fulfilled-logout', state);
                 return state = initialState;
             })
-            .addMatcher(api.endpoints.isLoggedIn.matchFulfilled, (state, action)=>{
+            .addMatcher(api.endpoints.isLoggedIn.matchFulfilled, (state, action) => {
                 console.log('fulfilled-session recovered', action);
-                state.name = action.payload.name;
-                state.username = action.payload.username;
-                state.email = action.payload.email;
-                state.birthday = action.payload.birthday;
-                state.profile.id = action.payload.profile.id;
-                state.profile.location = action.payload.profile.location;
-                state.profile.bio = action.payload.profile.bio;
-                state.profile.profile_picture = action.payload.profile.profile_picture;
-                state.habits = action.payload.habits;
+                const { name, username, email, birthday, profile, habits } = action.payload;
+                Object.assign(state, { name, username, email, birthday, profile, habits })
             })
             .addMatcher(api.endpoints.updateHabit.matchPending, (state, action) => {
                 console.log('pending', action);
             })
             .addMatcher(api.endpoints.updateHabit.matchFulfilled, (state, action) => {
                 console.log('fulfilled-updateHabit', action);
-                state.habits = state.habits.map(habit=> habit.id === action.payload.id ? action.payload : habit);
+                return {
+                    ...state, habits: state.habits.map(habit => {
+                        if (habit.id === action.payload.id) {
+                            return action.payload
+                        }
+                        return habit
+                    })
+                }
             })
             .addMatcher(api.endpoints.updateHabit.matchRejected, (state, action) => {
                 console.log('rejected', action);
@@ -71,16 +65,74 @@ export const slice = createSlice({
             })
             .addMatcher(api.endpoints.newHabit.matchFulfilled, (state, action) => {
                 console.log('fulfilled-newHabit', action);
-                state.habits = [...state.habits, action.payload.habit];
+                state.habits.push(action.payload)
             })
             .addMatcher(api.endpoints.newHabit.matchRejected, (state, action) => {
                 console.log('rejected', action);
             })
+            .addMatcher(api.endpoints.deleteHabit.matchPending, (state, action) => {
+                console.log('pending', action);
+            })
+            .addMatcher(api.endpoints.deleteHabit.matchFulfilled, (state, action) => {
+                console.log('fulfilled-deleteHabit', action);
+                const newHabits = state.habits.filter(habit=> habit.id !== action.payload.id)
+                state.habits = newHabits
+            })
+            .addMatcher(api.endpoints.deleteHabit.matchRejected, (state, action) => {
+                console.log('rejected', action);
+            })
             .addMatcher(api.endpoints.updateProfile.matchFulfilled, (state, action) => {
                 console.log('fulfilled-updateProfile', action);
-                state.profile.bio = action.payload.bio;
-                state.profile.location = action.payload.location;
-                return state
+                return {
+                    ...state, profile: {...action.payload}
+                }
+            })
+            .addMatcher(api.endpoints.newLog.matchFulfilled, (state, action) => {
+                console.log('fulfilled-newLog', action);
+                return {
+                    ...state, habits: state.habits.map(habit => {
+                        if (habit.id === action.payload.id) {
+                            return action.payload
+                        }
+                        return habit
+                    })
+                }
+            })
+            .addMatcher(api.endpoints.updateLog.matchPending, (state, action) => {
+                console.log('pending', action);
+            })
+            .addMatcher(api.endpoints.updateLog.matchFulfilled, (state, action) => {
+                console.log('fulfilled-updateLog', action);
+                return {
+                    ...state, habits: state.habits.map(habit => {
+                        if (habit.id === action.payload.habit_id) {
+                            habit.logs.map(log=>{
+                               if (log.id === action.payload.id){
+                                   return action.payload
+                               }
+                               return log
+                            })
+                        }
+                        return habit
+                    })
+                }
+            })
+            .addMatcher(api.endpoints.updateLog.matchRejected, (state, action) => {
+                console.log('rejected', action);
+            })
+            .addMatcher(api.endpoints.deleteLog.matchFulfilled, (state, action) => {
+                console.log('fulfilled-deleteLog', action);
+                return {
+                    ...state, habits: state.habits.map(habit => {
+                        if (habit.id === action.payload.id) {
+                            return action.payload
+                        }
+                        return habit
+                    })
+                }
+            })
+            .addMatcher(api.endpoints.deleteLog.matchRejected, (state, action) => {
+                console.log('rejected', action);
             })
     }
 })
